ace89439afd9b05675854ff6e7cafcef3ed337a5
FelixOS <puffis.stenersjo@gmail.com>
2024-09-16 14:38:28 +0200

    https://github.com/puffisbre/TwitterClone.git

--- main.js
+++ main.js
@@ -3,10 +3,22 @@ window.onload = function(){
     positionNmb = 0;
 }
 
+class Profile {
+    constructor(name, username, profileImg){
+        this.name = name;
+        this.username = username;
+        this.profileImg = profileImg;
+    }
+}
+
 function addTweet(){
+  let NewUser = new Profile("Felix", "Flikzerman", "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png");
   let parent = document.getElementById("feedContainer");
   let tweetTextId = document.getElementById("tweetText");
-  let tweetText = document.createElement("p");
+  let tweetContent = document.createElement("p");
+  let name = document.createElement("h2");
+  let username = document.createElement("h4");
+  let profileImg = document.createElement("img");
   let tweet = document.createElement("div");
   
   tweet.style.width = "80%";
@@ -17,9 +29,25 @@ function addTweet(){
   tweet.style.left = "10%"
   tweet.style.marginTop = "5%"
   tweet.style.order = positionNmb.toString();
+  name.style.position = "absolute";
+  name.style.left = "25%"
+  name.style.top = "-10%"
+  username.style.position = "absolute";
+  username.style.left = "33%"
+  username.style.top = "-8%"
+  tweetContent.style.position = "absolute";
+  tweetContent.style.left = "25%";
+  tweetContent.style.top = "25%";
+  tweetContent.style.fontSize = "25px"
   positionNmb--;
-  tweetText.innerHTML += tweetTextId.value;
-  tweet.appendChild(tweetText);
+  tweetContent.innerHTML += tweetTextId.value;
+  name.innerHTML += NewUser.name;
+  username.innerHTML += `${"@"+NewUser.username}`;
+  profileImg.src = NewUser.profileImg;
+  tweet.appendChild(profileImg);
+  tweet.appendChild(name);
+  tweet.appendChild(username);
+  tweet.appendChild(tweetContent);
   parent.appendChild(tweet);
   tweetTextId.value = "";
 }
