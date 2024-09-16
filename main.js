4f94f290bf1627a11dd8562c24d498422e95b1b3
FelixOS <puffis.stenersjo@gmail.com>
2024-09-16 17:22:20 +0200

    https://github.com/puffisbre/TwitterClone.git

--- main.js
+++ main.js
@@ -20,34 +20,45 @@ function addTweet(){
   let username = document.createElement("h4");
   let profileImg = document.createElement("img");
   let tweet = document.createElement("div");
-  
-  tweet.style.width = "80%";
+  //Styling the tweet
+  tweet.style.width = "100%";
   tweet.style.height = "20%";
-  tweet.style.border = "3px solid blue";
+  tweet.style.border = "3px solid gray";
   tweet.style.position = "relative";
   tweet.style.display = "flex";
-  tweet.style.left = "10%"
-  tweet.style.marginTop = "5%"
   tweet.style.order = positionNmb.toString();
+  tweet.style.overflow = "scroll";
+  tweet.style.overflowX = "hidden";
+  //Name styling
   name.style.position = "absolute";
   name.style.left = "25%"
   name.style.top = "-10%"
+  name.style.color = "white"
+  //Username styling
   username.style.position = "absolute";
   username.style.left = "33%"
   username.style.top = "-8%"
+  username.style.color = "gray"
+  //Content styling
   tweetContent.style.position = "absolute";
   tweetContent.style.left = "25%";
-  tweetContent.style.top = "25%";
-  tweetContent.style.fontSize = "25px"
+  tweetContent.style.top = "15%";
+  tweetContent.style.fontSize = "20px"
+  tweetContent.style.color = "white"
+  
+  //Position of tweet so newest tweet is on top
   positionNmb--;
+  //Adding text and profile image to the tweet
   tweetContent.innerHTML += tweetTextId.value;
   name.innerHTML += NewUser.name;
   username.innerHTML += `${"@"+NewUser.username}`;
   profileImg.src = NewUser.profileImg;
+  //Append content to tweet and then appending to parent container
   tweet.appendChild(profileImg);
   tweet.appendChild(name);
   tweet.appendChild(username);
   tweet.appendChild(tweetContent);
   parent.appendChild(tweet);
+  //Clears input field when tweeted
   tweetTextId.value = "";
 }
