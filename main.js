f6eb5840f998963d648942b26e4a9d54e071b5c4
FelixOS <puffis.stenersjo@gmail.com>
2024-09-18 14:28:52 +0200

    https://github.com/puffisbre/TwitterClone.git

--- main.js
+++ main.js
@@ -4,13 +4,15 @@ let tweet;
 let storageTweet;
 let btnId;
 let theModal;
+let tweetContent;
+let textToEdit = [];
+let activeModal;
+let activeParentTweet;
 window.onload = function(){
     positionNmb = 0;
     btnId = 0;
     parent = document.getElementById("feedContainer"); 
 }
-
-
 
 class Profile {
     constructor(name, username, profileImg){
@@ -22,19 +24,20 @@ class Profile {
 let NewUser = new Profile("Felix", "Flikzerman", "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png");
 function addTweet(){
   
-  
+  //#region Variables
   let parent = document.getElementById("feedContainer");
   tweet = document.createElement("div");
   let tweetTextId = document.getElementById("tweetText");
-  let tweetContent = document.createElement("p");
+  tweetContent = document.createElement("p");
   let name = document.createElement("h2");
   let username = document.createElement("h4");
   let profileImg = document.createElement("img");
   let removeBtn = document.createElement("button");
   let editBtn = document.createElement("button");
+  //#endregion
   
   
-  //Styling the tweet
+  //#region Styling the tweet
   tweet.style.width = "100%";
   tweet.style.height = "20%";
   tweet.style.border = "3px solid gray";
@@ -43,25 +46,32 @@ function addTweet(){
   tweet.style.order = positionNmb.toString();
   tweet.style.overflow = "scroll";
   tweet.style.overflowX = "hidden";
-  //Name styling
+  //#endregion
+
+  //#region Name styling
   name.style.position = "absolute";
   name.style.left = "25%"
-  name.style.top = "-10%"
+  name.style.top = "1%"
   name.style.color = "white"
-  //Username styling
+  //#endregion
+
+  //#region Username styling
   username.style.position = "absolute";
-  username.style.left = "33%"
-  username.style.top = "-8%"
+  username.style.left = "32%"
+  username.style.top = "4%"
   username.style.color = "gray"
-  //Content styling
+  //#endregion
+
+  //#region Content styling
   tweetContent.style.position = "absolute";
   tweetContent.style.left = "25%";
   tweetContent.style.top = "15%";
   tweetContent.style.fontSize = "20px"
   tweetContent.style.color = "white"
+  //#endregion
 
 
-  //Edit button management
+  //#region Edit button management
   editBtn.innerHTML = "Edit";
   editBtn.name = "editBtn";
   editBtn.style.position = "absolute";
@@ -71,9 +81,11 @@ function addTweet(){
   editBtn.style.weight = "10px"
   editBtn.style.backgroundColor = "green";
   editBtn.addEventListener("click", function() {
-    editTweet();
+    editTweet(this);
 });
-  //Remove button management
+//#endregion
+
+  //#region Remove button management
   btnId++;
   removeBtn.id = btnId.toString();
   removeBtn.innerHTML = "Delete";
@@ -87,15 +99,20 @@ function addTweet(){
   removeBtn.addEventListener("click", function() {
     removeTweet(this);
 });
+//#endregion
   
-  //Position of tweet so newest tweet is on top
+  //#region Position of tweet so newest tweet is on top
   positionNmb--;
-  //Adding text and profile image to the tweet
+  //#endregion
+
+  //#region Adding text and profile image to the tweet
   tweetContent.innerHTML += tweetTextId.value;
   name.innerHTML += NewUser.name;
   username.innerHTML += `${"@"+NewUser.username}`;
   profileImg.src = NewUser.profileImg;
-  //Append content to tweet and then appending to parent container
+  //#endregion
+
+  //#region Append content to tweet and then appending to parent container
   tweet.id = positionNmb.toString();
   tweet.appendChild(profileImg);
   tweet.appendChild(name);
@@ -104,33 +121,97 @@ function addTweet(){
   tweet.appendChild(removeBtn);
   tweet.appendChild(editBtn);
   parent.appendChild(tweet);
-  //Clears input field when tweeted
+  textToEdit.push(tweetContent);
+  //#endregion
+
+  //#region Clears input field when tweeted
   tweetTextId.value = "";
+  //#endregion
+
 }
 
-function editTweet(){
+function editTweet(para){
+
+ //#region Variables
 let backdrop = document.getElementById("bdrop");
+let profileImg = document.createElement("img");
+let parentForm = document.createElement("form");
+let editContent = document.createElement("input");
+let completeBtn = document.createElement("button");
 backdrop.style.display = "block";
 theModal = document.createElement("div");
-theModal.id = "theModal";
- theModal.style.display = "block";
+//#endregion
+
+//#region When pushing the edit button, i set the variable "activeParentTweet" to this buttons parent.
+activeParentTweet = para.parentNode;
+//#endregion
+
+//#region Modal style
+theModal.id = "activeModal";
+ theModal.style.display = "flex";
  theModal.style.position = "fixed";
  theModal.style.zIndex = "1";
  theModal.style.left = "30%";
  theModal.style.top = "4%";
- theModal.style.width = "39.5%";
+ theModal.style.width = "40%";
  theModal.style.height = "30%";
  theModal.style.overflow = "auto";
- theModal.style.backgroundColor = "gray";
- theModal.style.opacity = "0.9"
- parent.appendChild(theModal);
+ theModal.style.backgroundColor = "white";
+ theModal.style.borderRadius = "25px"
+ //#endregion
+
+ //#region Input style
+ editContent.style.width = "420px"
+ editContent.style.height = "95%"
+ editContent.style.marginTop = "1%"
+ editContent.style.fontSize = "25px"
+ //#endregion
+ 
+ //#region CompleteButton style
+ completeBtn.innerHTML = "Save";
+ completeBtn.name = "completeBtn";
+ completeBtn.style.position = "absolute";
+ completeBtn.style.left = "93%"
+ completeBtn.style.bottom = "1%"
+ completeBtn.style.height = "20px"
+ completeBtn.style.width = "45px"
+ completeBtn.style.borderRadius = "25px"
+ completeBtn.style.backgroundColor = "green";
+ completeBtn.addEventListener("click", function() {
+   completeEdit(this);
+   theModal.remove();
+   backdrop.style.display = "none";
+});
+//#endregion
+ 
+//#region Content in modal and appending children
+ profileImg.src = NewUser.profileImg;
+ //editContent.value = c.innerHTML;
+ parentForm.appendChild(editContent);
+ theModal.appendChild(completeBtn);
+ theModal.appendChild(profileImg);
+ theModal.appendChild(parentForm);
+ tweet.appendChild(theModal);
+ activeModal = document.getElementById("activeModal");
+ //#endregion
+ 
+ //#region Backdrop
  backdrop.addEventListener('click', function(event) {
   const clickModal = backdrop.contains(event.target);
   if(clickModal){
-    theModal.style.display = "none";
+    theModal.remove();
     backdrop.style.display = "none";
   }
 });
+//#endregion
+}
+
+function completeEdit(e){
+  let activeParent = activeParentTweet;
+  let activeModal = document.getElementById("activeModal");
+  let oldContent= activeParent.childNodes[3];
+  let newContent = activeModal.childNodes[2].childNodes[0];
+  oldContent.innerHTML = newContent.value;
 }
 
 function removeTweet(t){
